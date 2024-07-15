91e57e42b79684871da121f3624749d8cf21905a
mayujie <1009239228@qq.com>
2024-07-15 21:54:13 +0800

    event

---
+++ 2.event/1_events.js
@@ -0,0 +1,13 @@
+const EventEmitter = require("events");
+
+const emitter = new EventEmitter();
+function handleWhy(name, ...args) {
+  console.log("监听", name, ...args);
+}
+
+emitter.on("why", handleWhy); // 监听
+// emitter.off("why", handleWhy); //关闭
+
+setTimeout(() => {
+  emitter.emit("why", "why1", 18, 121);
+}, 2000);
