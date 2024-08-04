a22becf7be5a4a56dc9f8e28d8ec3fcd8925cdab
mayujie <1009239228@qq.com>
2024-08-04 20:46:03 +0800

    文件长传

--- 5_node_服务/13.文件上传.js
+++ 5_node_服务/13.文件上传.js
@@ -7,24 +7,23 @@ const port = 3000
 const server = http.createServer((req, res) => {
   // 设置编码 二进制数据
   req.setEncoding('binary')
-  const boundary = req.headers['content-type'].split(';')[1].trim().replace('boundary=', '')
-
+  // 'multipart/form-data; boundary=----WebKitFormBoundary0MjF8UwfnWyXMsba'
+  const boundary = req.headers['content-type'].split('; ')[1].trim().replace('boundary=', '')
   let formData =''
   req.on('data', (chunk) => {
-    console.log('chunk', chunk)
+    console.log(3333,chunk)
     formData += chunk
   })
   req.on('end', () => {
     const imageType = 'image/jpeg'
     const imageTypePosition = formData.indexOf(imageType) + imageType.length
     let imageData = formData.substring(imageTypePosition)
-
     // 替换空格
     imageData = imageData.replace('/^\s\s*/','')
     // 替换boundary
-    imageData = imageData.substring(0, imageData.indexOf(`--${boundary}--`))
+    imageData = imageData.substring(0, imageData.indexOf(`--${boundary}--`)).trim();
     // 存储
-    fs.writeFile('image.png', imageData,'binary', (err) => {
+    fs.writeFile('./image.jpg', imageData,'binary', (err) => {
       if (err) throw err
       console.log('Image saved!')
       res.writeHead(200, { 'Content-Type': 'text/plain' })
