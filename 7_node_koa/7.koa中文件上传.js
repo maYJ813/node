afabc6c2bbe4363e5c1c36fb1995f0dae3f84bb7
mayujie <mayujie@ovopark.com>
2024-08-17 13:35:16 +0800

    koa 文件上传

---
+++ 7_node_koa/7.koa中文件上传.js
@@ -0,0 +1,41 @@
+const Koa = require('koa');
+const port = 3000;
+const Router = require('@koa/router');
+const multer = require('@koa/multer');
+
+const app = new Koa();
+
+// const upload = multer({dest: 'uploads/'}); // 默认路径
+// 自定义文件路径
+const upload =multer({
+  storage:multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + '_' + file.originalname)
+    }
+  })
+})
+const router = new Router({prefix: '/upload'});
+
+// 单文件上传
+router.post('/avatar', upload.single('avatar'), async (ctx, next) => {
+  // console.log(ctx.request.file);
+  ctx.body = 'File uploaded successfully!';
+})
+// 多文件上传
+router.post('/photos', upload.array('photos', 12), async (ctx, next) => {
+  console.log(ctx.request.files);
+  ctx.body = 'Files uploaded successfully!';
+})
+
+// 错误处理
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${ port }`)
+})
+
