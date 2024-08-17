51c31c0536dcd0b01694493033f2e3c47f4ca4e7
mayujie <mayujie@ovopark.com>
2024-08-17 14:21:49 +0800

    koa 相应方式

---
+++ 7_node_koa/9.koa相应数据类型.js
@@ -0,0 +1,31 @@
+const Koa = require('koa')
+const port = 3000
+const app = new Koa()
+
+const Router = require('@koa/router')
+const fs = require('fs');
+
+const router = new Router({prefix: '/users'})
+
+router.get('/', (ctx, next) => {
+  // 1.字符串类型
+  // ctx.body='get user'
+  // 2.buffer
+  // ctx.body = Buffer.from('hello world')
+//   3.流
+//   const readStream = fs.createReadStream('./uploads/1.jpg')
+//   ctx.type = 'image/jpeg' // 告诉客户端返回的数据类型
+//   ctx.body = readStream
+//   4.body arr\object
+//   ctx.status =201
+  ctx.body = JSON.stringify({code: 0, name: 'jack', age: 18})
+//   5 null
+//   ctx.body = null //不输出任何内容
+})
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${ port }`)
+})
