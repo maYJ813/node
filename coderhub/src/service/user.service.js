83bf48fa3c9f63637791c488f7ea80b1bc7412ff
mayujie <1009239228@qq.com>
2024-08-25 16:11:23 +0800

    koa mysql 连接 调用 创建用户之前查询

--- coderhub/src/service/user.service.js
+++ coderhub/src/service/user.service.js
@@ -3,16 +3,22 @@ const connection = require('../app/database');
 
 class UserService {
 
+  // 创建用户
   async create(user){
     //   1.获取用户
     const { name,password} = user
-    console.log(name,password)
     //   2.拼接
     const statement = 'INSERT INTO `user` ( name, password ) VALUES ( ?,?);'
-    console.log('statement', statement)
     // 3.执行sql语句  ? 将被name password 替换
-    return await connection.execute(statement, [name, password])
+    let [result] = await connection.execute(statement, [name, password])
+    return result;
 
+  }
+//   查询用户
+  async findUserByName(name){
+    const statement = 'SELECT * FROM `user` WHERE name = ?;'
+    let [values,fields] = await connection.execute(statement, [name])
+    return values;
   }
 }
 
